c2523e0cc012dfaff2d298178cea9df3d32a93f0
seanpoff <129460816+seanpoff@users.noreply.github.com>
2023-04-11 01:45:48 -0700

    Create inches.c
    The exercise prompt says to convert centimeters to inches or inches to centimeters, which is completed by line 19; however, I wanted to provide a foot and inches output (like 5ft 10 inches).
    
    To do so, I took the total amount of inches and divided by 12 (70.08/12 = 5.84), then converted that value to an integer so I could be left with just the whole number value (5). From there, I used the float value, and subtracted it from the integer I just created to leave me with just the decimal value (5.84-5 = 0.84). This decimal value, of course, isn't the actual inches in a foot, so I multiply by 12 to get the actual inches rather than a fraction of a foot (0.84*12 = 10.08).
    
    I'm sure there are better ways to do this, especially alternatives to scanf, converting a float to an int, and combining different data types. Based on what I've learned so far, this is what I've come up with. I'll be sure to add a revised version when I come across better ways to write this program.

---
+++ inches.c
@@ -0,0 +1,36 @@
+/* inches.c -- converts height in cm to in */
+
+#include <stdio.h>
+int main(void)
+{
+    // initialize variables
+    int i_heightcm=0, i_feet_value=0;
+    float f_heightin=0, f_inches_value=0, f_raw_height=0, f_raw_inches=0;
+    
+    // ask user for their height in cm
+    printf("Enter your height in cm. ");
+    scanf("%d", &i_heightcm);
+    
+    // convert cm to inches
+    f_heightin = i_heightcm / 2.54;
+    
+    // display height in inches
+    printf("\n%d centimeters is %.2f inches tall.\n",
+           i_heightcm, f_heightin);
+    
+    // get the raw height value (5.89ft)
+    f_raw_height = f_heightin / 12;
+    
+    // separate the integer value from the decimal value
+    i_feet_value = (int)f_raw_height;
+    f_raw_inches = f_raw_height - i_feet_value;
+    
+    // convert foot decimal to actual inches
+    f_inches_value = f_raw_inches * 12;
+    
+    // display actual height using feet and inches
+    printf("That means you're %dft, %.1f inches tall!\n",
+           i_feet_value, f_inches_value);
+    
+    return 0;
+}
