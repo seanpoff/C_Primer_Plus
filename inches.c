9f5caa2ac0e13fe9a7c2e8a2cbd88bee29cdfd6a
seanpoff <129460816+seanpoff@users.noreply.github.com>
2023-05-07 01:19:24 -0700

    Update inches.c
    Added a function to handle the centimeter to feet and inches conversion.
    Added conditional statement to handle invalid user input.
    Changed the names of variables to simplify program.

--- inches.c
+++ inches.c
@@ -1,41 +1,55 @@
-/* inches.c -- converts height in cm to in */
+/* inches.c -- converts height in cm to height in inches */
 #include <stdio.h>
+
+// declare function prototype
+int height_out(void);
+
+// declare and initialize global variables
+int height_in = 0;
+int feet_out = 0;
+
+float raw_height = 0;
+float inches_out = 0;
 
 int main(void)
 {
-    // initialize variables
-    int i_heightcm = 0;
-    int i_feet_value = 0;
-    
-    float f_heightin = 0;
-    float f_raw_height = 0;
-    float f_raw_inches = 0;
-    float f_inches_value = 0;
-    
-    // ask user for their height in cm
-    printf("Enter your height in cm. ");
-    scanf("%d", &i_heightcm);
-    
-    // convert cm to inches
-    f_heightin = (i_heightcm / 2.54);
-    
-    // display height in inches
-    printf("\n%d centimeters is %.2f inches tall.\n",
-           i_heightcm, f_heightin);
-    
-    // get the raw height value (5.89ft)
-    f_raw_height = (f_heightin / 12);
-    
-    // separate the integer value from the decimal value
-    i_feet_value = (int)f_raw_height;
-    f_raw_inches = (f_raw_height - i_feet_value);
-    
-    // convert foot decimal to actual inches
-    f_inches_value = (f_raw_inches * 12);
-    
-    // display actual height using feet and inches
-    printf("That means you're %dft, %.1f inches tall!\n",
-           i_feet_value, f_inches_value);
-    
+    // declare and initialize local variable
+    int status = 0;
+
+    // prompt for user input
+    printf("\nEnter your height in cm. ");
+    status = scanf("%d", &height_in);
+
+    // if valid entry, complete conversion
+    if (status == 1)
+    {
+        // call function to complete conversion
+        height_out();
+    }
+
+    // if invalid entry, terminate program
+    else {
+        printf("\nSorry, that was an invalid entry.\n\n");
+    }
+
+    return 0;
+}
+
+// conversion function
+int height_out(void)
+{
+    // centimeter to feet
+    raw_height = ((height_in / 2.54) / 12);
+
+    // isolate just the integer of feet value
+    feet_out = (int)raw_height;
+
+    // take remaining decimal value and convert to inches
+    inches_out = (raw_height - feet_out) * 12;
+
+    // display results
+    printf("\n%d centimeters is %dft, %.1f inches tall!\n\n",
+        height_in, feet_out, inches_out);
+
     return 0;
 }
