7cb245ee5001a28f10d4cad105c363954306a3c2
seanpoff <129460816+seanpoff@users.noreply.github.com>
2023-05-11 19:19:06 -0700

    Update 4.c
    Made adjustments to display substitutions for periods and exclamation marks individually as well as the total sub count.

--- chapter_7/4.c
+++ chapter_7/4.c
@@ -1,7 +1,8 @@
 /* Using if else statments, write a program that reads input up to #, 
  * replaces each period with an exclamation mark, replaces each 
  * exclamation mark initially present with two exclamation marks,
- * and reports at the end the number of sustitutions it has made. */
+ * and reports at the end the number of sustitutions it has made.
+ */
 #include <stdio.h>
 
 int main(void)
@@ -9,7 +10,8 @@ int main(void)
     // initialize variables
     char ch = 'a';
 
-    int sub = 0;
+    int e_sub = 0;
+    int p_sub = 0;
 
     // prompt for user input
     printf("Enter some text for me to analyze ('#' to quit).\n\n");
@@ -19,12 +21,12 @@ int main(void)
 
         // replace periods with exclamation marks
         if ('.' == ch) {
-            sub++;
+            p_sub++;
             putchar('!');
 
         // print two exclamations marks for each inital exclamation mark
         } else if ('!' == ch) {
-            sub++;
+            e_sub++;
             printf("!!");
 
         // print the rest of the original input
@@ -34,7 +36,11 @@ int main(void)
     }
 
     // display number of substitutions made
-    printf("\n\nI made a total of %d substitutions.\n", sub);
+    printf("\n\nI made a total of %d substitutions.\n", (p_sub + e_sub));
+    printf("Exclamation marks: %d\n", e_sub);
+    printf("Periods: %d\n", p_sub);
+
+    printf("\nThanks for playing!\n\n");
 
     return 0;
 }
