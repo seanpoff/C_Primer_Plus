8f501a97b8a2c23748b04add82fa18752eca9d35
seanpoff <129460816+seanpoff@users.noreply.github.com>
2023-05-12 22:08:46 -0700

    Update 3.c

--- chapter_7/3.c
+++ chapter_7/3.c
@@ -18,34 +18,42 @@ int main(void)
     printf("Enter a list of integers for me to analyze ('0' to quit).\n\n");
 
     // while value is entered but not 0
-    while (((scanf("%d", &num)) == 1) && (num != 0)) {
-        
+    while (((scanf("%d", &num)) == 1) && (num != 0)) 
+    {        
         // increase count for even numbers
-        if ((num % 2) == 0) {
+        if ((num % 2) == 0) 
+        {
             e_count++;
             e_sum += num;
         
         // increase count for odd numbers
-        } else {
+        } 
+        else 
+        {
             o_count++;
             o_sum += num;
         }
     }
 
     // if both types are present, display results
-    if (o_count && e_count) {
+    if (o_count && e_count) 
+    {
         printf("\nNumber of evens:     Avg even:     "
             "Number of odds:     Avg odd:\n");
         printf("%8d %16.1f %16d %17.1f\n", e_count, (float)(e_sum / e_count), 
             o_count, (float)(o_sum / o_count));
 
     // if only evens present, display results
-    } else if (e_count) {
+    } 
+    else if (e_count) 
+    {
         printf("\nNumber of evens:     Average:     No odd values\n");
         printf("%8d %17.1f\n", e_count, (float)(e_sum / e_count));
 
     // if only odds present, display results
-    } else {
+    } 
+    else 
+    {
         printf("\nNumber of odds:     Average:     No even values.\n");
         printf("%8d %16.1f\n", o_count, (float)(o_sum / o_count));
     }
