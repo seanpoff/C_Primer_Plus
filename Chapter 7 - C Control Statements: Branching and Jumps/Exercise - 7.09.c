6516a49f3aefde92078cdc4a617cbcf9d0fd7527
seanpoff <129460816+seanpoff@users.noreply.github.com>
2023-05-14 23:29:06 -0700

    Update and rename 9.c to Exercise - 7.09.c

--- Chapter 7 - C Control Statements: Branching and Jumps/Exercise - 7.09.c
+++ Chapter 7 - C Control Statements: Branching and Jumps/Exercise - 7.09.c
@@ -2,6 +2,7 @@
  * all the prime numbers smaller than or equal to that number.
  */
 #include <stdio.h>
+
 /*******************************************************************************
  * Function Main
  */
@@ -15,7 +16,7 @@ int main(void)
 
     printf("Enter a positive integer: ");
 
-    while ((1 == (scanf("%d", &limit))) && (0 < limit))
+    while ((1 == (scanf("%d", limit))) && (0 < limit))
     {
         if (1 > limit)
         {
@@ -23,7 +24,7 @@ int main(void)
         }
         else
         {
-            printf("Here are the primes through %d:\n\n", limit);
+            printf("Here are the list of primes through %d.\n\n", limit);
 
             for (num = 2; num <= limit; num++)
             {
@@ -35,14 +36,14 @@ int main(void)
                     }
                 }
 
-                if (1 == isprime)
+                if (1 == (isprime))
                 {
                     printf("%d ", num);
                 }
             }
         }
 
-        printf("\n\nEnter another number (any other key to quit).\n");
+        printf("Enter another number (any other key to quit).\n\n");
     }
 
     return 0;
