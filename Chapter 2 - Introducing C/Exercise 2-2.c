3a8df030578649bf2d06d90c485d96dc1a5c6208
Sean Poffenbarger <129460816+seanpoff@users.noreply.github.com>
2023-05-15 21:27:03 -0700

    Create Exercise 2-2.c

---
+++ Chapter 2 - Introducing C/Exercise 2-2.c
@@ -0,0 +1,14 @@
+/* Write a program to print your name and address */
+#include <stdio.h>
+
+/*******************************************************************************
+ * Function Main
+ */
+
+int main(void)
+{
+    printf("\nSean Poffenbarger\n");
+    printf("123 Main St., Anywhere, Oh, 43002\n\n");
+
+    return 0;
+}
