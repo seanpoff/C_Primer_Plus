7bd04eb454c7f8c4f603bc83e2abc9315672cc17
Sean Poffenbarger <129460816+seanpoff@users.noreply.github.com>
2023-05-15 22:51:59 -0700

    Create Exercise 3-3.c

---
+++ Chapter 3 - Data and C/Exercise 3-3.c
@@ -0,0 +1,18 @@
+/* Write a program that sounds an alert and then prints the following text:
+ *
+ * Startled by the sudden sound, Sally shouted,
+ * "By the Great Pumpkin, what was that!"
+ */
+#include <stdio.h>
+
+/*******************************************************************************
+ * Function Main
+ */
+
+int main(void)
+{
+    printf("\007\nStartled by the sudden sound, Sally shouted,\n");
+    printf("\"By the Great Pumpkin, what was that!\"\n\n");
+
+    return 0;
+}
